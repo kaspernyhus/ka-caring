2fa5d63aedb8b2d377c2ce4592aa7c9c14e26ec7
Kasper Nyhus <kanyhus@gmail.com>
2020-03-08 14:24:03 +0100

    bookings error

--- bookings/views.py
+++ bookings/views.py
@@ -1,38 +1,38 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-import pickle
-import datetime
-
-
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# import pickle
+# import datetime
 
 
-def setupAPI():
-  flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
-  creds = flow.run_local_server(port=0)
-
-  pickle.dump(creds, open('token.pkl', 'wb'))
-
-  print('------------ SETUP API ------------')
+# SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
+# def setupAPI():
+#   flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", SCOPES)
+#   creds = flow.run_local_server(port=0)
 
-def bookings(request):
-    setupAPI()
+#   pickle.dump(creds, open('token.pkl', 'wb'))
+
+#   print('------------ SETUP API ------------')
+
+
+
+# def bookings(request):
+#     setupAPI()
     
-    creds = pickle.load(open('token.pkl', 'tb'))
+#     creds = pickle.load(open('token.pkl', 'tb'))
 
-    service = build('calendar', 'v3', credentials=creds)
+#     service = build('calendar', 'v3', credentials=creds)
 
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+#     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+#     print('Getting the upcoming 10 events')
+#     events_result = service.events().list(calendarId='primary', timeMin=now,
+#                                         maxResults=10, singleEvents=True,
+#                                         orderBy='startTime').execute()
+#     events = events_result.get('items', [])
 
-    print(events)
+#     print(events)
     
-    return render(request, 'bookings.html', context=events)
+#     return render(request, 'bookings.html', context=events)
