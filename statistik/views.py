18c24714896abeaeb62f2b93daacbc37d46a496b
Kasper Nyhus <kanyhus@gmail.com>
2020-03-13 18:37:11 +0100

    stats

--- statistik/views.py
+++ statistik/views.py
@@ -4,12 +4,8 @@ from db_functions.db_data import get_total_tankning, get_current_km
 def show_stats(request):
   km = get_current_km()
   km = km - 114293 #km ved overtagelse
-  tankning = get_total_tankning()
+  tankning = 1
   pris_pr_km = km / tankning
-
-  print(km)
-  print(tankning)
-  print(pris_pr_km)
 
   context = {'pris_pr_km': pris_pr_km}
 
