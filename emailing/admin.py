8156050551257377e079cbfd5676a00161a1e6ed
Kasper Nyhus <kanyhus@gmail.com>
2020-03-21 14:00:17 +0100

    email pref admin reg

--- emailing/admin.py
+++ emailing/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import UserPreferences, EmailQue
 
 
-admin.site.register(UserPreferences)
+class EmailPrefAdmin(admin.ModelAdmin):
+    list_display = ('id','monthly_saldo', 'udgift_oprettet', 'tankning_oprettet', 'tur_oprettet')
+
+admin.site.register(UserPreferences, EmailPrefAdmin)
 admin.site.register(EmailQue)
